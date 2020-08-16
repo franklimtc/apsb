4212a0aca4335853226c2f61380b093f61b9d6bd
franklimtc <franklimtc@outlook.com>
2020-08-15 22:58:07 -0300

    Atualização do projeto de banco.

--- APSBSolution/Database/dbo/Stored Procedures/EXC_ProfissionalAuto.sql
+++ APSBSolution/Database/dbo/Stored Procedures/EXC_ProfissionalAuto.sql
@@ -6,20 +6,40 @@ AS
     BEGIN
 	   SET NOCOUNT ON;
 	   DECLARE @Result BIT= 0;
+	   DECLARE @idProfissional INT= 0;
+	   SELECT @idProfissional = idProfissional
+	   FROM   tbProfissionais
+	   WHERE  DBO.FN_TokenAuto(IdProfissional, cdDataCriacao) = @Token;
 	   BEGIN TRY
-		  ALTER TABLE tbClinicaProfissional NOCHECK CONSTRAINT ALL;
-		  ALTER TABLE tbEndereco NOCHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionaisBancos NOCHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionaisDados NOCHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionalArquivo NOCHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionalRepasse NOCHECK CONSTRAINT ALL;
-		  DELETE tbProfissionais WHERE  DBO.FN_TokenAuto(IdProfissional, cdDataCriacao) = @Token;
-		  ALTER TABLE tbClinicaProfissional CHECK CONSTRAINT ALL;
-		  ALTER TABLE tbEndereco CHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionaisBancos CHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionaisDados CHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionalArquivo CHECK CONSTRAINT ALL;
-		  ALTER TABLE tbProfissionalRepasse CHECK CONSTRAINT ALL;
+		  IF @idProfissional IS NOT NULL
+			 BEGIN
+				ALTER TABLE tbClinicaProfissional NOCHECK CONSTRAINT ALL;
+				ALTER TABLE tbEndereco NOCHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionaisBancos NOCHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionaisDados NOCHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionalArquivo NOCHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionalRepasse NOCHECK CONSTRAINT ALL;
+				DELETE tbProfissionais
+				WHERE  IdProfissional = @idProfissional;
+				DELETE tbClinicaProfissional
+				WHERE  idProfissional = @idProfissional;
+				DELETE tbEndereco
+				WHERE  idProfissional = @idProfissional;
+				DELETE tbProfissionaisBancos
+				WHERE  idProfissional = @idProfissional;
+				DELETE tbProfissionaisDados
+				WHERE  idProfissional = @idProfissional;
+				DELETE tbProfissionalArquivo
+				WHERE  idProfissional = @idProfissional;
+				DELETE tbProfissionalRepasse
+				WHERE  idProfissional = @idProfissional;
+				ALTER TABLE tbClinicaProfissional CHECK CONSTRAINT ALL;
+				ALTER TABLE tbEndereco CHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionaisBancos CHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionaisDados CHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionalArquivo CHECK CONSTRAINT ALL;
+				ALTER TABLE tbProfissionalRepasse CHECK CONSTRAINT ALL;
+			 END;
 		  SET @Result = 1;
 	   END TRY
 	   BEGIN CATCH
