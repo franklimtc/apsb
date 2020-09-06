743f5c48298a7c8a98b129c73f2b0a4dc05f0ca2
franklimtc <franklimtc@outlook.com>
2020-09-06 12:31:01 -0300

    Editando nome da aplicação (APSDF X APSB)

--- APSBSolution/Site/Site.Master.designer.cs
+++ APSBSolution/Site/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace Site
         protected global::System.Web.UI.HtmlControls.HtmlForm @default;
 
         /// <summary>
+        /// Controle AppTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor AppTitle;
+
+        /// <summary>
         /// Controle MainContent.
         /// </summary>
         /// <remarks>
