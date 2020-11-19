e3b658ccc640db2f10ebd2729b8e6e4c02d73a4d
franklimtc <franklimtc@outlook.com>
2020-11-19 20:56:12 -0300

    Teste com WebJob de arquivamento

---
+++ APSBSolution/WebJob/ArquivarOperacoes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ArquivarOperacoes
+{
+    class Program
+    {
+        static string JobName = ConfigurationManager.AppSettings["JobName"];
+
+        static void Main(string[] args)
+        {
+            Default db = new Default();
+            tbParametros parametros = db.tbParametros.ToList().FirstOrDefault();
+
+            DateTime dtFiltro = DateTime.Now.AddDays(DateTime.Now.Day * -1).AddMonths(parametros.ArquivarOperacoesEmMeses * -1).AddDays(1);
+
+            tbLogJobs log = new tbLogJobs();
+            log.JobName = JobName;
+
+            try
+            {
+                var result = db.ARQ_Operacoes(dtFiltro);
+
+                log.logs = $"Foram arquivadas {result} operações.";
+                log.status = "C";
+
+                db.tbLogJobs.Add(log);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                log.status = "E";
+                log.logs = $"{ex.Message}";
+                db.SaveChanges();
+            }
+           
+        }
+    }
+}
