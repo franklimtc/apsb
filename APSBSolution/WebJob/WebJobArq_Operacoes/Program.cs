b4cec7074f4f406b061563baa03b128d6b8a006b
franklimtc <franklimtc@outlook.com>
2021-04-29 08:38:18 -0300

    correções

--- APSBSolution/WebJob/WebJobArq_Operacoes/Program.cs
+++ APSBSolution/WebJob/WebJobArq_Operacoes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebJobArq_Operacoes
@@ -11,11 +12,28 @@ namespace WebJobArq_Operacoes
     {
         static void Main(string[] args)
         {
-            APSB db = new APSB();
-            var qtdMonths = ConfigurationManager.AppSettings["Months"];
-            DateTime dtfim = DateTime.Now.AddMonths(int.Parse(qtdMonths) * -1);
-            db.ARQ_Operacoes(dtfim, "WebJobArquivamento");
-            db.SaveChanges();
+            while (true)
+            {
+                using (var db = new APSB())
+                {
+                    try
+                    {
+                        Console.WriteLine("Iniciando arquivamento de operações!");
+                        var qtdMonths = ConfigurationManager.AppSettings["Months"];
+                        DateTime dtfim = DateTime.Now.AddMonths(int.Parse(qtdMonths) * -1);
+                        var qtd = db.ARQ_Operacoes(dtfim, "WebJobArquivamento");
+                        //db.SaveChanges();
+                        Console.WriteLine($"{qtd} registros arquivados com sucesso!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                   
+                }
+                Thread.Sleep(new TimeSpan(24, 0, 0));
+            }
+           
         }
     }
 }
